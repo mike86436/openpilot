7690a17b2b610ee7db71514de10e4495c609e4fe
mike8643 <98910897+mike8643@users.noreply.github.com>
2026-08-01 19:56:51 -0400

    Update hondacan.py

--- opendbc_repo/opendbc/car/honda/hondacan.py
+++ opendbc_repo/opendbc/car/honda/hondacan.py
@@ -66,7 +66,7 @@ def create_brake_command(packer, CAN, apply_brake, pump_on, pcm_override, pcm_ca
     "AEB_STATUS": 0,
   }
 
-    if honda_flags & HondaFlags.HYBRID:
+  if honda_flags & HondaFlags.HYBRID:
     values["COMPUTER_BRAKE_HYBRID"] = apply_brake
     values["BRAKE_PUMP_REQUEST_HYBRID"] = brake_rq
   else:
