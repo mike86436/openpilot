b024b172eb7f78c6937ed00601f1181680d9e212
mike8643 <98910897+mike8643@users.noreply.github.com>
2026-08-17 16:12:03 -0400

    Update carcontroller.py

--- opendbc_repo/opendbc/car/honda/carcontroller.py
+++ opendbc_repo/opendbc/car/honda/carcontroller.py
@@ -304,8 +304,6 @@ class CarController(CarControllerBase):
 
     # steer torque is converted back to CAN reference (positive when steering right)
     apply_torque = int(np.interp(-limited_torque * self.params.STEER_MAX, self.params.STEER_LOOKUP_BP, self.params.STEER_LOOKUP_V))
-    if CS.out.steeringPressed:
-      apply_torque = 0
 
     # Send CAN commands
     can_sends = []
